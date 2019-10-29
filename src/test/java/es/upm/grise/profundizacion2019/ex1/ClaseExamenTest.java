089365d713d30aaebbef23bbe561502873ebfe82
IsmaaelMunozAz <izzmael95@gmail.com>
2019-10-29 15:48:50 +0100

    examen_Ismael_Munoz_Aztout

--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -1,14 +1,27 @@
 package es.upm.grise.profundizacion2019.ex1;
-
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class ClaseExamenTest {
 
+	ClaseExamen claseExamen=new ClaseExamen();
+	//Test 1: a=5,b=3
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		float result=0.0f;
+		int a=5;
+		int b=3;
+		assertEquals(result, claseExamen.metodoAProbar(a, b));
 	}
-
+	
+	//Test 1: a=3,b=3
+	@Test
+	public void test2() {
+		float result=0.0f;
+		int a=3;
+		int b=3;
+		assertEquals(result, claseExamen.metodoAProbar(a, b));
+	}
 }
